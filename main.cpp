43691e471da13196294c5cf2c0d42a234858c98b
arosennin <arek1arek4@onet.pl>
2019-11-06 23:22:07 +0100

    zmiana plangry

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
     cout << "-----------------------------------PINK RPG------------------------------------" << endl;
+    system("pause");
+    system("cls");
+
     return 0;
 }
